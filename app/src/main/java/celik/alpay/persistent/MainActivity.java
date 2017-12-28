a05af4f186c32d8133d7202562f7f8e1a0c088dc
alpaycelik <alpay.celik@bil.omu.edu.tr>
2017-12-28 23:33:49 +0200

    Bundle ayarlandı.

--- app/src/main/java/celik/alpay/persistent/MainActivity.java
+++ app/src/main/java/celik/alpay/persistent/MainActivity.java
@@ -2,12 +2,40 @@ package celik.alpay.persistent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b;
+    TextView tv;
+    int say;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.TextView);
+        if(savedInstanceState != null){
+            say = savedInstanceState.getInt("sayac");
+            tv.setText("sayaç: "+say);
+        }
+        else {
+            say = 0;
+        }
+        b = (Button) findViewById(R.id.button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                say++;
+                tv.setText("sayaç: "+say);
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("sayac", say);
     }
 }
